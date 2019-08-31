e470d7523b5bcb5b1df55404a0b2a030e65338e6
Rhonda Tinkham <rtinkham0408@gmail.com>
2019-08-31 23:11:06 +0000

    added chef new and edit features

---
+++ test/integration/chefs_edit_test.rb
@@ -0,0 +1,29 @@
+require 'test_helper'
+
+class ChefsEditTest < ActionDispatch::IntegrationTest
+
+   def setup
+   @chef = Chef.create!(chefname: "patty", email: "patty@gmail.com", password: "password", password_confirmation: "password")
+  end
+
+  test 'reject an invalid edit' do
+    get edit_chef_path(@chef)
+    assert_template 'chefs/edit'
+    patch chef_path(@chef), params: { chef: {chefname: ' ', email: 'patty@gmail.com' }}
+    assert_template 'chefs/edit'
+    assert_select 'h2.card-title'
+    assert_select 'div.card-body'
+  end
+
+  test 'accept valid signup' do
+    get edit_chef_path(@chef)
+    assert_template 'chefs/edit'
+    patch chef_path(@chef), params: { chef: {chefname: 'patty1', email: 'patty1@gmail.com' }}
+    assert_redirected_to @chef
+    assert_not flash.empty?
+    @chef.reload
+    assert_match 'patty1', @chef.chefname
+    assert_match 'patty1@gmail.com', @chef.email
+  end
+
+end
