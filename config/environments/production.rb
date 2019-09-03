b037a5c2f6c45d84f3b9bb9b53a761a42de61109
Rhonda Tinkham <rtinkham0408@gmail.com>
2019-09-03 22:05:50 +0000

    fixed error

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,5 +92,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   config.web_socket_server_url = "wss://recipe-friends-share.herokuapp.com/cable"
-  config.action_cable.allowed_request_origins = ['https//recipe-friends-share.herokuapp.com', 'http://recipe-friends-share.herokuapp.com']
+  config.action_cable.allowed_request_origins = ['https://recipe-friends-share.herokuapp.com', 'http://recipe-friends-share.herokuapp.com']
 end
